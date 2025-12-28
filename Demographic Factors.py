fc8086335b872201200a8bba96b3946107e6eff5
MissNurin <nurinnuha363@gmail.com>
2025-12-28 23:07:40 +0800

    Update and rename Demographic Factors to Demographic Factors.py

---
+++ Demographic Factors.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Demografic Factors Page"
+)
+visualise = st.Page('Demographic Factors.py', title='Demographic Factors on Impulse Buying in TikTok', icon=":shopping:")
+    )
+
+pg.run()
